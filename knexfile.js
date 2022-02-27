96b5a96da8384b4011584fcb0652182059642971
Aguziendu Ugochukwu <ugochukwua@flexipgroup.com>
2022-02-27 20:41:00 +0100

    bug fix

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,7 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+console.log(process.env);
 // Update with your config settings.
 
 /**
@@ -35,9 +39,16 @@ module.exports = {
   production: {
     client: 'mysql',
     connection: process.env.DATABASE_URL,
+    // connection: {
+    //   connectionString: process.env.DATABASE_URL,
+    //   host: process.env.DB_HOST,
+    //   database: process.env.DB_DATABASE,
+    //   db: process.env.DB_DATABASE,
+    //   user: process.env.DB_USER,
+    //   password: process.env.DB_PASSWORD,
+    // },
     migrations: {
       directory: './database/migrations/',
-      tableName: 'knex_migrations',
     },
   },
 
